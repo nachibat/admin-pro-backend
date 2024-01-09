1181252121efa1efd6ff8eaf07ae3bd646461e4c
Osvaldo Torrez <nachi2083@gmail.com>
2024-01-09 18:06:53 -0300

    Fin seccion 10 - Auth

---
+++ controllers/auth.js
@@ -0,0 +1,45 @@
+const { response } = require('express');
+const bcrypt = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
+const { generarJWT } = require('../helpers/jwt');
+
+const login = async (req, res = response) => {
+    const { email, password } = req.body;
+    try {
+        // Verificar email
+        const usuarioDB = await Usuario.findOne({ email });
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Email no encontrado'
+            });
+        }
+        
+        // Verificar contraseña
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msn: 'Password no valido'
+            });
+        }
+
+        //Generar el token
+        const token = await generarJWT(usuarioDB.id);
+        res.json({
+            ok: true,
+            token
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
+}
+
+module.exports = {
+    login
+}
